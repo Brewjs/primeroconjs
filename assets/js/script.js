5430f60eccc32de0b976b992aa62c049fd5b1cec
Nicolas Labbe <nicoo.labbe@gmail.com>
2022-02-17 20:43:37 -0300

    cambios en mensajes de consola

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("funcionado")
+console.log();
+
+document.write();
+
+alert("mensaje informativo");
+prompt("mensaje emergente para solicitar datos");
+confirm("mensaje emergente par sssoliciar confirmacion");
+
